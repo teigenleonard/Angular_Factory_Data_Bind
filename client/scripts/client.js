493466b10f3300872c196183f8617b86fdb2065d
Teigen Leonard <teigenl@gmail.com>
2017-04-11 23:33:50 -0500

    display working

---
+++ client/scripts/client.js
@@ -0,0 +1,45 @@
+var myApp = angular.module( 'myApp', []);
+
+myApp.controller( 'InputController', ['$scope','MovieService', function($scope, MovieService){
+  // console.log('InputController');
+  $scope.newMovie = MovieService.movie;
+  console.log(Movieservice.movie);
+  $scope.addMovie = MovieService.addMovie;
+}]);
+
+myApp.controller( 'DisplayController', ['$scope', 'MovieService', function($scope, MovieService){
+  // console.log('DisplayController');
+  $scope.allTheMoviesObject = MovieService.allTheMoviesObject;
+  console.log($scope.allTheMoviesObject.length);
+}]);
+
+
+myApp.factory('MovieService', [function(){
+
+  var movie = {
+    title : '',
+    description : '',
+    director : '',
+    runTime : ''
+  };
+
+  var movieArray = [];
+  var allTheMoviesObject = {
+    movieArray : movieArray
+  };
+
+function addMovie(){
+  // console.log('addMovie');
+  var savedMovie = angular.copy(movie);
+  movieArray.push(savedMovie);
+  console.log(movieArray);
+}
+
+
+return {
+  movie : movie,
+  addMovie : addMovie,
+  allTheMoviesObject : allTheMoviesObject
+};
+
+}]);
